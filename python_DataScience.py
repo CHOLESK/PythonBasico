e69e3e217421fa1509ce7082fef0bb45024b1980
Luis del Aguila <laguilaes@gmail.com>
2020-03-15 15:40:10 +0100

    hg

--- python_DataScience.py
+++ python_DataScience.py
@@ -179,6 +179,133 @@ poly2.fit_transform(X)
 poly2 = PolynomialFeatures(2, interaction_only=True)
 poly2.fit_transform(X)
 
+#%% Distribuciones
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from distutils.version import LooseVersion
+from scipy.stats import norm
+from sklearn.neighbors import KernelDensity
+
+# `normed` is being deprecated in favor of `density` in histograms
+if LooseVersion(matplotlib.__version__) >= '2.1':
+    density_param = {'density': True}
+else:
+    density_param = {'normed': True}
+
+# ----------------------------------------------------------------------
+# Plot the progression of histograms to kernels
+np.random.seed(1)
+N = 20
+X = np.concatenate((np.random.normal(0, 1, int(0.3 * N)),
+                    np.random.normal(5, 1, int(0.7 * N))))[:, np.newaxis]
+X_plot = np.linspace(-5, 10, 1000)[:, np.newaxis]
+bins = np.linspace(-5, 10, 10)
+
+fig, ax = plt.subplots(2, 2, sharex=True, sharey=True)
+fig.subplots_adjust(hspace=0.05, wspace=0.05)
+
+# histogram 1
+ax[0, 0].hist(X[:, 0], bins=bins, fc='#AAAAFF', **density_param)
+ax[0, 0].text(-3.5, 0.31, "Histogram")
+
+# histogram 2
+ax[0, 1].hist(X[:, 0], bins=bins + 0.75, fc='#AAAAFF', **density_param)
+ax[0, 1].text(-3.5, 0.31, "Histogram, bins shifted")
+
+# tophat KDE
+kde = KernelDensity(kernel='tophat', bandwidth=0.75).fit(X)
+log_dens = kde.score_samples(X_plot)
+ax[1, 0].fill(X_plot[:, 0], np.exp(log_dens), fc='#AAAAFF')
+ax[1, 0].text(-3.5, 0.31, "Tophat Kernel Density")
+
+# Gaussian KDE
+kde = KernelDensity(kernel='gaussian', bandwidth=0.75).fit(X)
+log_dens = kde.score_samples(X_plot)
+ax[1, 1].fill(X_plot[:, 0], np.exp(log_dens), fc='#AAAAFF')
+ax[1, 1].text(-3.5, 0.31, "Gaussian Kernel Density")
+
+for axi in ax.ravel():
+    axi.plot(X[:, 0], np.full(X.shape[0], -0.01), '+k')
+    axi.set_xlim(-4, 9)
+    axi.set_ylim(-0.02, 0.34)
+
+for axi in ax[:, 0]:
+    axi.set_ylabel('Normalized Density')
+
+for axi in ax[1, :]:
+    axi.set_xlabel('x')
+
+# ----------------------------------------------------------------------
+# Plot all available kernels
+X_plot = np.linspace(-6, 6, 1000)[:, None]
+X_src = np.zeros((1, 1))
+
+fig, ax = plt.subplots(2, 3, sharex=True, sharey=True)
+fig.subplots_adjust(left=0.05, right=0.95, hspace=0.05, wspace=0.05)
+
+
+def format_func(x, loc):
+    if x == 0:
+        return '0'
+    elif x == 1:
+        return 'h'
+    elif x == -1:
+        return '-h'
+    else:
+        return '%ih' % x
+
+for i, kernel in enumerate(['gaussian', 'tophat', 'epanechnikov',
+                            'exponential', 'linear', 'cosine']):
+    axi = ax.ravel()[i]
+    log_dens = KernelDensity(kernel=kernel).fit(X_src).score_samples(X_plot)
+    axi.fill(X_plot[:, 0], np.exp(log_dens), '-k', fc='#AAAAFF')
+    axi.text(-2.6, 0.95, kernel)
+
+    axi.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
+    axi.xaxis.set_major_locator(plt.MultipleLocator(1))
+    axi.yaxis.set_major_locator(plt.NullLocator())
+
+    axi.set_ylim(0, 1.05)
+    axi.set_xlim(-2.9, 2.9)
+
+ax[0, 1].set_title('Available Kernels')
+
+# ----------------------------------------------------------------------
+# Plot a 1D density example
+N = 100
+np.random.seed(1)
+X = np.concatenate((np.random.normal(0, 1, int(0.3 * N)),
+                    np.random.normal(5, 1, int(0.7 * N))))[:, np.newaxis]
+
+X_plot = np.linspace(-5, 10, 1000)[:, np.newaxis]
+
+true_dens = (0.3 * norm(0, 1).pdf(X_plot[:, 0])
+             + 0.7 * norm(5, 1).pdf(X_plot[:, 0]))
+
+fig, ax = plt.subplots()
+ax.fill(X_plot[:, 0], true_dens, fc='black', alpha=0.2,
+        label='input distribution')
+colors = ['navy', 'cornflowerblue', 'darkorange']
+kernels = ['gaussian', 'tophat', 'epanechnikov']
+lw = 2
+
+for color, kernel in zip(colors, kernels):
+    kde = KernelDensity(kernel=kernel, bandwidth=0.5).fit(X)
+    log_dens = kde.score_samples(X_plot)
+    ax.plot(X_plot[:, 0], np.exp(log_dens), color=color, lw=lw,
+            linestyle='-', label="kernel = '{0}'".format(kernel))
+
+ax.text(6, 0.38, "N={0} points".format(N))
+
+ax.legend(loc='upper left')
+ax.plot(X[:, 0], -0.005 - 0.01 * np.random.random(X.shape[0]), '+k')
+
+ax.set_xlim(-4, 9)
+ax.set_ylim(-0.02, 0.4)
+plt.show()
+
 #%% Quitar variables con poca variancia
 # library(caret)
 # preProcValues <- preProcess(iris[,-5], method = "nzv")
@@ -3071,6 +3198,255 @@ ax4.set_title('Clustering at 2.0 epsilon cut\nDBSCAN')
 
 plt.tight_layout()
 plt.show()
+
+
+#%% OUTLIERS
+
+import time
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+from sklearn import svm
+from sklearn.datasets import make_moons, make_blobs
+from sklearn.covariance import EllipticEnvelope
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+
+matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
+
+# Example settings
+n_samples = 300
+outliers_fraction = 0.15
+n_outliers = int(outliers_fraction * n_samples)
+n_inliers = n_samples - n_outliers
+
+# define outlier/anomaly detection methods to be compared
+anomaly_algorithms = [
+    ("Robust covariance", EllipticEnvelope(contamination=outliers_fraction)),
+    ("One-Class SVM", svm.OneClassSVM(nu=outliers_fraction, kernel="rbf",
+                                      gamma=0.1)),
+    ("Isolation Forest", IsolationForest(contamination=outliers_fraction,
+                                         random_state=42)),
+    ("Local Outlier Factor", LocalOutlierFactor(
+        n_neighbors=35, contamination=outliers_fraction))]
+
+# Define datasets
+blobs_params = dict(random_state=0, n_samples=n_inliers, n_features=2)
+datasets = [
+    make_blobs(centers=[[0, 0], [0, 0]], cluster_std=0.5,
+               **blobs_params)[0],
+    make_blobs(centers=[[2, 2], [-2, -2]], cluster_std=[0.5, 0.5],
+               **blobs_params)[0],
+    make_blobs(centers=[[2, 2], [-2, -2]], cluster_std=[1.5, .3],
+               **blobs_params)[0],
+    4. * (make_moons(n_samples=n_samples, noise=.05, random_state=0)[0] -
+          np.array([0.5, 0.25])),
+    14. * (np.random.RandomState(42).rand(n_samples, 2) - 0.5)]
+
+# Compare given classifiers under given settings
+xx, yy = np.meshgrid(np.linspace(-7, 7, 150),
+                     np.linspace(-7, 7, 150))
+
+plt.figure(figsize=(len(anomaly_algorithms) * 2 + 3, 12.5))
+plt.subplots_adjust(left=.02, right=.98, bottom=.001, top=.96, wspace=.05,
+                    hspace=.01)
+
+plot_num = 1
+rng = np.random.RandomState(42)
+
+for i_dataset, X in enumerate(datasets):
+    # Add outliers
+    X = np.concatenate([X, rng.uniform(low=-6, high=6,
+                       size=(n_outliers, 2))], axis=0)
+
+    for name, algorithm in anomaly_algorithms:
+        t0 = time.time()
+        algorithm.fit(X)
+        t1 = time.time()
+        plt.subplot(len(datasets), len(anomaly_algorithms), plot_num)
+        if i_dataset == 0:
+            plt.title(name, size=18)
+
+        # fit the data and tag outliers
+        if name == "Local Outlier Factor":
+            y_pred = algorithm.fit_predict(X)
+        else:
+            y_pred = algorithm.fit(X).predict(X)
+
+        # plot the levels lines and the points
+        if name != "Local Outlier Factor":  # LOF does not implement predict
+            Z = algorithm.predict(np.c_[xx.ravel(), yy.ravel()])
+            Z = Z.reshape(xx.shape)
+            plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='black')
+
+        colors = np.array(['#377eb8', '#ff7f00'])
+        plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[(y_pred + 1) // 2])
+
+        plt.xlim(-7, 7)
+        plt.ylim(-7, 7)
+        plt.xticks(())
+        plt.yticks(())
+        plt.text(.99, .01, ('%.2fs' % (t1 - t0)).lstrip('0'),
+                 transform=plt.gca().transAxes, size=15,
+                 horizontalalignment='right')
+        plot_num += 1
+
+plt.show()
+
+
+
+#%% Outliers One Class SVM
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+from sklearn import svm
+
+xx, yy = np.meshgrid(np.linspace(-5, 5, 500), np.linspace(-5, 5, 500))
+# Generate train data
+X = 0.3 * np.random.randn(100, 2)
+X_train = np.r_[X + 2, X - 2]
+# Generate some regular novel observations
+X = 0.3 * np.random.randn(20, 2)
+X_test = np.r_[X + 2, X - 2]
+# Generate some abnormal novel observations
+X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
+
+# fit the model
+clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+clf.fit(X_train)
+y_pred_train = clf.predict(X_train)
+y_pred_test = clf.predict(X_test)
+y_pred_outliers = clf.predict(X_outliers)
+n_error_train = y_pred_train[y_pred_train == -1].size
+n_error_test = y_pred_test[y_pred_test == -1].size
+n_error_outliers = y_pred_outliers[y_pred_outliers == 1].size
+
+# plot the line, the points, and the nearest vectors to the plane
+Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.title("Novelty Detection")
+plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7), cmap=plt.cm.PuBu)
+a = plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
+plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='palevioletred')
+
+s = 40
+b1 = plt.scatter(X_train[:, 0], X_train[:, 1], c='white', s=s, edgecolors='k')
+b2 = plt.scatter(X_test[:, 0], X_test[:, 1], c='blueviolet', s=s,
+                 edgecolors='k')
+c = plt.scatter(X_outliers[:, 0], X_outliers[:, 1], c='gold', s=s,
+                edgecolors='k')
+plt.axis('tight')
+plt.xlim((-5, 5))
+plt.ylim((-5, 5))
+plt.legend([a.collections[0], b1, b2, c],
+           ["learned frontier", "training observations",
+            "new regular observations", "new abnormal observations"],
+           loc="upper left",
+           prop=matplotlib.font_manager.FontProperties(size=11))
+plt.xlabel(
+    "error train: %d/200 ; errors novel regular: %d/40 ; "
+    "errors novel abnormal: %d/40"
+    % (n_error_train, n_error_test, n_error_outliers))
+plt.show()
+
+#%% Outliers Isolation forest
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import IsolationForest
+
+rng = np.random.RandomState(42)
+
+# Generate train data
+X = 0.3 * rng.randn(100, 2)
+X_train = np.r_[X + 2, X - 2]
+# Generate some regular novel observations
+X = 0.3 * rng.randn(20, 2)
+X_test = np.r_[X + 2, X - 2]
+# Generate some abnormal novel observations
+X_outliers = rng.uniform(low=-4, high=4, size=(20, 2))
+
+# fit the model
+clf = IsolationForest(max_samples=100, random_state=rng)
+clf.fit(X_train)
+y_pred_train = clf.predict(X_train)
+y_pred_test = clf.predict(X_test)
+y_pred_outliers = clf.predict(X_outliers)
+
+# plot the line, the samples, and the nearest vectors to the plane
+xx, yy = np.meshgrid(np.linspace(-5, 5, 50), np.linspace(-5, 5, 50))
+Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.title("IsolationForest")
+plt.contourf(xx, yy, Z, cmap=plt.cm.Blues_r)
+
+b1 = plt.scatter(X_train[:, 0], X_train[:, 1], c='white',
+                 s=20, edgecolor='k')
+b2 = plt.scatter(X_test[:, 0], X_test[:, 1], c='green',
+                 s=20, edgecolor='k')
+c = plt.scatter(X_outliers[:, 0], X_outliers[:, 1], c='red',
+                s=20, edgecolor='k')
+plt.axis('tight')
+plt.xlim((-5, 5))
+plt.ylim((-5, 5))
+plt.legend([b1, b2, c],
+           ["training observations",
+            "new regular observations", "new abnormal observations"],
+           loc="upper left")
+plt.show()
+
+#%% Local Outlier Factor
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import LocalOutlierFactor
+
+
+np.random.seed(42)
+
+# Generate train data
+X_inliers = 0.3 * np.random.randn(100, 2)
+X_inliers = np.r_[X_inliers + 2, X_inliers - 2]
+
+# Generate some outliers
+X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
+X = np.r_[X_inliers, X_outliers]
+
+n_outliers = len(X_outliers)
+ground_truth = np.ones(len(X), dtype=int)
+ground_truth[-n_outliers:] = -1
+
+# fit the model for outlier detection (default)
+clf = LocalOutlierFactor(n_neighbors=20, contamination=0.1)
+# use fit_predict to compute the predicted labels of the training samples
+# (when LOF is used for outlier detection, the estimator has no predict,
+# decision_function and score_samples methods).
+y_pred = clf.fit_predict(X)
+n_errors = (y_pred != ground_truth).sum()
+X_scores = clf.negative_outlier_factor_
+
+plt.title("Local Outlier Factor (LOF)")
+plt.scatter(X[:, 0], X[:, 1], color='k', s=3., label='Data points')
+# plot circles with radius proportional to the outlier scores
+radius = (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min())
+plt.scatter(X[:, 0], X[:, 1], s=1000 * radius, edgecolors='r',
+            facecolors='none', label='Outlier scores')
+plt.axis('tight')
+plt.xlim((-5, 5))
+plt.ylim((-5, 5))
+plt.xlabel("prediction errors: %d" % (n_errors))
+legend = plt.legend(loc='upper left')
+legend.legendHandles[0]._sizes = [10]
+legend.legendHandles[1]._sizes = [20]
+plt.show()
+
+
+
 
 
 
