78a4224cdb93572a49583ee4c66a939063343251
Luis del Aguila <laguilaes@gmail.com>
2020-03-15 20:56:15 +0100

    Update redes_neuronales.py

--- redes_neuronales.py
+++ redes_neuronales.py
@@ -225,11 +225,127 @@ row = [0.00632,18.00,2.310,0,0.5380,6.5750,65.20,4.0900,1,296.0,15.30,396.90,4.9
 yhat = model.predict([row])
 print('Predicted: %.3f' % yhat)
 
-#%%
+#%% Convolutional neural network
 
+# example of a cnn for image classification
+from numpy import unique
+from numpy import argmax
+from tensorflow.keras.datasets.mnist import load_data
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPool2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+import matplotlib.pyplot as plt
+# load dataset
+(x_train, y_train), (x_test, y_test) = load_data()
+# plot first few images
+for i in range(25):
+	# define subplot
+	pyplot.subplot(5, 5, i+1)
+	# plot raw pixel data
+	pyplot.imshow(trainX[i], cmap=pyplot.get_cmap('gray'))
+# show the figure
+pyplot.show()
+# reshape data to have a single channel
+x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], x_train.shape[2], 1))
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], x_test.shape[2], 1))
+# determine the shape of the input images
+in_shape = x_train.shape[1:]
+# determine the number of classes
+n_classes = len(unique(y_train))
+print(in_shape, n_classes)
+# normalize pixel values
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+# define model
+model = Sequential()
+model.add(Conv2D(32, (3,3), activation='relu', kernel_initializer='he_uniform', input_shape=in_shape))
+model.add(MaxPool2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dropout(0.5))
+model.add(Dense(n_classes, activation='softmax'))
+# define loss and optimizer
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# fit the model
+model.fit(x_train, y_train, epochs=10, batch_size=128, verbose=0)
+# evaluate the model
+loss, acc = model.evaluate(x_test, y_test, verbose=0)
+print('Accuracy: %.3f' % acc)
+# make a prediction
+image = x_train[0]
+yhat = model.predict([[image]])
+print('Predicted: class=%d' % argmax(yhat))
 
+#%% Recurrent Neural Networks
 
+# lstm for time series forecasting
+from numpy import sqrt
+from numpy import asarray
+from pandas import read_csv
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+import matplotlib.pyplot as plt
+import tensorflow as tf
+ 
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return asarray(X), asarray(y)
+ 
+# load the dataset
+path = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/monthly-car-sales.csv'
+df = read_csv(path, header=0, index_col=0, squeeze=True)
+plt.plot(df)
+# retrieve the values
+values = df.values.astype('float32')
+# specify the window size
+n_steps = 5
+# split into samples
+X, y = split_sequence(values, n_steps)
+# reshape into [samples, timesteps, features]
+X = X.reshape((X.shape[0], X.shape[1], 1))
+# split into train/test
+n_test = 12
+X_train, X_test, y_train, y_test = X[:-n_test], X[-n_test:], y[:-n_test], y[-n_test:]
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+# define model
+model = Sequential()
+model.add(LSTM(100, activation='relu', kernel_initializer='he_normal', input_shape=(n_steps,1)))
+model.add(Dense(50, activation='relu', kernel_initializer='he_normal'))
+model.add(Dense(50, activation='relu', kernel_initializer='he_normal'))
+model.add(Dense(1))
+# compile the model
+model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+# fit the model
+model.fit(X_train, y_train, epochs=350, batch_size=32, verbose=2, validation_data=(X_test, y_test))
+# evaluate the model
+mse, mae = model.evaluate(X_test, y_test, verbose=0)
+print('MSE: %.3f, RMSE: %.3f, MAE: %.3f' % (mse, sqrt(mse), mae))
+# make a prediction
+row = asarray([18024.0, 16722.0, 14385.0, 21342.0, 17180.0]).reshape((1, n_steps, 1))
+yhat = model.predict(row)
+print('Predicted: %.3f' % (yhat))
+#Summary model
+model.summary()
+# plot the model
+from keras.utils import plot_model
+import pydot
+plot_model(model, 'model.png', show_shapes=True)
+from IPython.display import SVG
+from keras.utils import model_to_dot
 
-
-
-
+SVG(model_to_dot(model).create(prog='dot', format='svg'))
